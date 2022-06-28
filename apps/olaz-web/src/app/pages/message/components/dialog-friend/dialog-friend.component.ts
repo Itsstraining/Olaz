5d22b599e9b005068352947aa86c31a1bec5bb1b
Dat Nguyen <ddatnguyen97@gmail.com>
2022-06-28 19:17:16 +0700

    add-friend-function

--- apps/olaz-web/src/app/pages/message/components/dialog-friend/dialog-friend.component.ts
+++ apps/olaz-web/src/app/pages/message/components/dialog-friend/dialog-friend.component.ts
@@ -1,6 +1,9 @@
+/* eslint-disable @typescript-eslint/member-ordering */
 /* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable @angular-eslint/no-empty-lifecycle-method */
 import { Component, OnInit } from '@angular/core';
+// import { collection, collectionData, addDoc, Firestore, getDoc, doc, setDoc} from '@angular/fire/firestore'
+// import { updateDoc } from '@firebase/firestore';
 
 @Component({
   selector: 'olaz-dialog-friend',
@@ -11,4 +14,28 @@ export class DialogFriendComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+  
+  email= "";
+
+  users = [
+    {
+      email: "ford@gmail.com"
+    },
+    {
+      email: "lexus@gmail.com"
+    },
+    {
+      email: "honda@gmail.com"
+    },
+    {
+      email: "kia@gmail.com"
+    }
+  ]
+
+  // findUser() {
+  //   for(let i =0; i<= this.users.length;i++){
+  //     if( )
+  //   }
+  // }
+
 }
