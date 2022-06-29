321b97ba4d0629c44a7ebd5217a518104160fba5
a@gamil.com <a@gamil.com>
2022-06-29 21:12:49 +0700

    router to pages: todo, message, call, profile

--- apps/task-management/src/app/app.module.ts
+++ apps/task-management/src/app/app.module.ts
@@ -3,10 +3,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoController } from './controllers/todo/todo.controller';
+import { TodoService } from './services/todo/todo.service';
+import { TaskService } from './services/task/task.service';
+import { TaskController } from './controllers/task/task.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, TodoController],
-  providers: [AppService],
+  controllers: [AppController, TodoController, TaskController],
+  providers: [AppService, TodoService, TaskService],
 })
 export class AppModule {}
--- apps/task-management/src/app/controllers/todo/todo.controller.ts
+++ apps/task-management/src/app/controllers/todo/todo.controller.ts
@@ -2,3 +2,4 @@ import { Controller } from '@nestjs/common';
 
 @Controller('todo')
 export class TodoController {}
+
