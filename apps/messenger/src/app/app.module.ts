9f865ca563689f87efcfe262d36c254028c2ab40
Trong Pham <trong.phamtranduc@gmail.com>
2022-07-06 11:19:53 +0700

    config server messenger

--- apps/messenger/src/app/app.module.ts
+++ apps/messenger/src/app/app.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserController } from './controllers/user/user.controller';
-import { UserService } from './services/user/user.service';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
 
 @Module({
   imports: [],
   controllers: [AppController, UserController],
   providers: [AppService, UserService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ apps/messenger/src/app/user/user.controller.ts
@@ -0,0 +1,59 @@
+/* eslint-disable no-empty */
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { Body, Controller, Get, HttpException, HttpStatus, Param, Post, Query } from '@nestjs/common';
+import { UserService } from '../user/user.service';
+@Controller('user')
+export class UserController {
+
+    constructor(
+        private UserService: UserService
+    ) { }
+
+    @Get('get-all')
+    async getUsers() {
+        try {
+            return await this.UserService.getUsers();
+        } catch (error) {
+            return new HttpException(error.message, HttpStatus.BAD_REQUEST)
+        }
+    }
+
+    @Post('add-friend')
+    async addFriend(@Body() body) {
+        try {
+            return this.UserService.toggleRequest(body.check, body.frID, body.myID);
+        } catch (error) {
+            return new HttpException(error.message, HttpStatus.BAD_REQUEST)
+        }
+    }
+
+    @Get('get-email')
+    gerUser(@Query('email') email: string) {
+        const users = this.UserService.getUserByEmail()
+        // for(let i = 0; i<users.length; i++){
+        //     if(users[i].email == email ){
+        //         return users[i];
+        //     }
+        // }
+        // return "Không có kết quả nào!"
+        // const newArr =  users.some( ai => ai.email.includes(email));
+        const newArr = users.filter(
+            (user) => {
+                return user.email
+                    .toLocaleLowerCase()
+                    .includes(email.toLocaleLowerCase())
+            })
+        return newArr;
+    }
+
+    @Get(':id')
+    async getUserById(@Param('id') id: string) {
+        try {
+            return await this.UserService.getUserById(id);
+        } catch (error) {
+            return new HttpException(error.message, HttpStatus.BAD_REQUEST)
+        }
+    }
+
+
+}
