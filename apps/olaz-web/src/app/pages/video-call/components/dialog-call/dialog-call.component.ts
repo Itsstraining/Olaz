03ac5cb9c0627b3b053b257a28345d151d98d7a6
trungdoublelift <trungdoublelift@gmail.com>
2022-07-28 18:44:38 +0700

    added calling audio

--- apps/olaz-web/src/app/pages/video-call/components/dialog-call/dialog-call.component.ts
+++ apps/olaz-web/src/app/pages/video-call/components/dialog-call/dialog-call.component.ts
@@ -4,7 +4,7 @@ import { Router } from '@angular/router';
 /* eslint-disable prefer-const */
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { Component, Inject, OnInit } from '@angular/core';
-import {MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { VideoService } from '../../../../services/video-call/video.service';
 @Component({
   selector: 'olaz-dialog-call',
@@ -12,19 +12,30 @@ import { VideoService } from '../../../../services/video-call/video.service';
   styleUrls: ['./dialog-call.component.css']
 })
 export class DialogCallComponent implements OnInit {
+  audio: any;
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private route: Router, public videoSrv: VideoService) {
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data:any,private route:Router,public videoSrv:VideoService ) {
-
-   }
+  }
 
   ngOnInit(): void {
+    this.audio = <HTMLAudioElement>document.getElementById('audio');
+    this.audio.src = '../../../../../assets/audios/incoming-call.wav';
+    this.audio.load();
+    this.audio.play();
+
+    this.audio.loop = true;
+
 
   }
   async answerCall() {
+    this.audio.pause();
+    this.audio.currentTime=0;
     this.route.navigate([`ownspace/call/call/${this.data.idRoom}`]);
   }
-   deleteCall(){
-     this.videoSrv.delData(this.data.idRoom).subscribe(()=>{
-     });
+  deleteCall() {
+    this.audio.pause();
+    this.audio.currentTime=0;
+    this.videoSrv.delData(this.data.idRoom).subscribe(() => {
+    });
   }
 }
